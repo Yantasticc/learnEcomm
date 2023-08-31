8cb94bab5c56ec1b46502c2fa71719e4572ba514
Yantasticc <yashkalamkhede2002@gmail.com>
2023-08-31 12:13:01 +0530

    undo

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Homepage.css'
 import Sidenav from './navigation/Sidenav'
+import Products from './products/products'
 
 function Homepage() {
   return (
@@ -8,6 +9,9 @@ function Homepage() {
         <div className='homepage'>
             <div className='homepage__nav'>
                 <Sidenav />
+            </div>
+            <div className='homepage__products'>
+                <Products/>
             </div>
         </div>
     </div>
---
+++ src/products/products.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './products.css'
+
+function products() {
+  return (
+    <div>
+        <div className='products__left'>products</div>
+        <div className='products__right'></div>
+    </div>
+  )
+}
+
+export default products
